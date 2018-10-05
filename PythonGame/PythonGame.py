f6548279a8c5cf3cde4de1fdb5be57739f2cda51
PETREKILLAH <Japanboysushi@gmail.com>
2018-10-05 00:05:56 -0400

    fixed walking out of room crashes and added npcs

--- PythonGame/PythonGame.py
+++ PythonGame/PythonGame.py
@@ -1,10 +1,21 @@
 import keyboard
 import string
 import time
+import math
 from os import system
 gameOn = True
 shouldMapUpdate = True
 currentRoom = 0
+#↑ ↓ ↖ ↗ ↘ ↙
+def distance(x1,y1,x2,y2):#Returns the distance between two points
+    return math.sqrt( (x2-x1) ** 2 ) + ( (y2-y1) ** 2 )
+class Config:
+    DirectionEnabled = True
+    DiagonalDirection = True
+    DiagonalMovement = True
+    WrapScreen = False
+    
+
 
 class Player:
     def __init__(self,x,y,maxHealth,moveDelay = 2):
@@ -14,6 +25,8 @@ class Player:
         self.health = self.maxHealth
         self.moveDelay = moveDelay
         self.moveCounter = 0
+        self.direction = 0
+
 class NPC:
     def __init__(self,x,y,maxHealth,moveDelay = 2,disposition = "HATE"):#I WANT TO IMPORT AND FIGURE OUT ENUMS REEEEEEEEEEEEEEE
         self.x = x
@@ -22,12 +35,42 @@ class NPC:
         self.health = self.maxHealth
         self.moveDelay = moveDelay
         self.moveCounter = 0
+        self.direction = 0
         self.disposition = disposition
-    
-  #NOTE TO SELF WHEN EDITING AGAIN: I LAST LEFT OFF EDITING THE COLLISION. THINGS ARE COLLIDING, BUT ONLY APPROACHING FROM LEFT AND RIGHT
-  #ITS BECAUSE I NEED TO CHECK ACROSS ROWS WHICH IS REALLY A PAIN IN THE DICK
+        self.agro = 10
+    def act(self):
+        if self.disposition == "HATE":
+            if distance(player.x,player.y,self.x,self.y) < 10:
+                if self.x > player.x and self.y > player.y:
+                    moveObject(self,-1,-1)
+                elif self.x < player.x and self.y < player.y:
+                    moveObject(self,1,1)
+                elif self.x < player.x and self.y > player.y:
+                    moveObject(self,1,-1)
+                elif self.x > player.x and self.y < player.y:
+                    moveObject(self,-1,1)
+                elif self.x > player.x:
+                    moveObject(self,-1,0)
+                elif self.x < player.x:
+                    moveObject(self,1,0)
+                elif self.y > player.y:
+                    moveObject(self,0,-1)
+                elif self.y < player.y:
+                    moveObject(self,0,1)
+                else:
+                    pass # The enemy is on the player
+
+                
+            
+
+
+
 player = Player(20,10,100,7)
-npc0 = NPC(40,10,100)
+npc0 = NPC(40,9,100,14)
+npc1 = NPC(20,9,100,14)
+npc = {
+    0:[npc0,npc1]
+    }
 room = {
     0:"""
 --------------------------------------------------------------------
@@ -36,6 +79,10 @@ room = {
 ------------------------------█-------------------------------------
 ------------------------------█-------------------------------------
 ------------------------------███████-------------------------------
+------------------------------------█-------------------------------
+------------------------------------█-------------------------------
+------------------------------------█-------------------------------
+------------------------------------█-------------------------------
 --------------------------------------------------------------------
 --------------------------------------------------------------------
 --------------------------------------------------------------------
@@ -44,39 +91,49 @@ room = {
 --------------------------------------------------------------------
 --------------------------------------------------------------------
 --------------------------------------------------------------------
---------------------------------------------------------------------
---------------------------------------------------------------------
---------------------------------------------------------------------
---------------------------------------------------------------------
-    """,
+""",
     1:"""
 
 """
 }
 
-def getRoomLen(room):#REMINDER THAT THIS FUNCTION MUST BE RUN EVERYTIME WE CHANGE ROOMS
+def getRoomDimensions(room):#REMINDER THAT THIS FUNCTION MUST BE RUN EVERYTIME WE CHANGE ROOMS
     
     room = room.lstrip("\n")
-    return room.find("\n")
+    return [room.find("\n"),room.count("\n")]
 
-roomLen = getRoomLen(room[currentRoom])
+roomLen = getRoomDimensions(room[currentRoom])[0]
+roomWid = getRoomDimensions(room[currentRoom])[1]
 
 
 def checkCollision(x,y):
     localRoom = room[currentRoom]
     localRoom = localRoom.replace("\n","")
-    return localRoom[x+y*roomLen]#THIS FORMULA NEEDS TO BE FIXED FOR LEAVING THROUGH THE SOUTH AND TOWARDS THE ORIGIN
+    if x < 0 or y < 0:
+        return "-"
+    elif x > roomLen-1 or y > roomWid-1:
+        return "-"
+    else:
+        return localRoom[x+y*roomLen]#THIS FORMULA NEEDS TO BE FIXED FOR LEAVING THROUGH THE SOUTH AND TOWARDS THE ORIGIN
 
+def checkForNpc(x,y):
+    for mob in npc[currentRoom]:
+        if x == mob.x and y == mob.y:
+            return False
+        else:
+            return True
 
 def moveObject(object,x,y):
     global shouldMapUpdate
     localRoom = room[currentRoom]
+
     if object.moveCounter > 0: #Are we allowed to move by according to cooldown?(No)
         object.moveCounter -= 1
+
     else:#Are we allowed to move according to cooldown?(Yup)
 
-        if checkCollision(object.x + x, object.y + y) == "-":#Will we be hitting anything if we move there?(No)
-
+        if checkCollision(object.x + x, object.y + y) == "-" and checkForNpc(object.x + x, object.y + y):#Will we be hitting anything if we move there?(No)
+            
             #start count down for move delay
             object.moveCounter = object.moveDelay
 
@@ -92,30 +149,35 @@ def renderMap():
     row.pop(0)
 
     #Draw Player
-    temp = list(row[player.y])
-    if player.x < len(temp) and player.x > -1:
-        temp[player.x] = "@"
-        row[player.y] = "".join(temp)
+    if player.x <= roomLen - 1 and player.y <= roomWid - 1 and player.x >= 0 and player.y >= 0:
+        temp = list(row[player.y])
+        if player.x < len(temp) and player.x > -1:
+            temp[player.x] = "@"
+            row[player.y] = "".join(temp)
 
-    #Draw Enemy (TO DO THIS, I NEED TO MAKE A SYTEM OF STORING ENEMIES IN ROOMS AND HAVING THEM DELETE WHEN THEY DIE)
-    #I WILL MOST LIKELY HAVE MAKE ROOMS STORED IN AN ARRAY INSIDE OF THE DICTIONARY CONTAINING ALL OF THEIR POSITIONS,
-    #HEALTH VALUES, ETC)
+    for mob in npc[currentRoom]:
+        temp = list(row[mob.y])
+        temp[mob.x] = "#"
+        row[mob.y] = "".join(temp)
+        
     localRoom = "\n".join(row)
     print(localRoom)
 
 def catchPlayerInput():
     global gameOn
     if keyboard.is_pressed('w'):#up
-        if keyboard.is_pressed('a'):
+        if keyboard.is_pressed('a'):#up left
             moveObject(player,-1,-1)
+            player.direction = 135
         elif keyboard.is_pressed('d'):
-            moveObject(player,1,-1)
+            moveObject(player,1,-1)#up right
+            player.direction = 45
         else:
             moveObject(player,0,-1)
     elif keyboard.is_pressed('s'):#down
-        if keyboard.is_pressed('a'):
+        if keyboard.is_pressed('a'):#down left
             moveObject(player,-1,1)
-        elif keyboard.is_pressed('d'):
+        elif keyboard.is_pressed('d'):#down left
             moveObject(player,1,1)
         else:
             moveObject(player,0,1)
@@ -127,9 +189,8 @@ def catchPlayerInput():
         gameOn = False
 
 
-#checkCollsion(0,1)
-#print(getRoomLen(room[0]))
-#time.sleep(10)
+
+
 while(gameOn):
     
     if shouldMapUpdate == True:
@@ -137,8 +198,12 @@ while(gameOn):
         renderMap()
         shouldMapUpdate = False
         #print(player.x,player.y)
+
     catchPlayerInput()
 
+    for mob in npc[currentRoom]:
+        mob.act()
+        
     
     #Game Speed(The lower it is, the faster)
     time.sleep(0.005)
